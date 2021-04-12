7181fb489a9654b8720a0d9aa3a720399768deea
dev-kiri <72954146+dev-kiri@users.noreply.github.com>
2021-04-12 13:41:12 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ class NaverTV {
     private readonly USER_AGENT: string = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36';
     
     constructor(private url: string) {
-        void 0;
+        
     }
 
     private uuidv4() {
